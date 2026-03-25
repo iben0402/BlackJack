b1a48724393e09b853d93b822c6d7b9cc6cea25e
Iwona Bendig <iwona.bbbb@gmail.com>
2026-03-25 13:20:09 +0100

    create deck class

---
+++ backend/src/main/java/com/iwonabendig/blackjack/model/Deck.java
@@ -0,0 +1,33 @@
+package com.iwonabendig.blackjack.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private final List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                Card card = new Card(suit, rank);
+                cards.add(card);
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("Cannot deal from an empty deck");
+        }
+        return cards.removeFirst();
+    }
+
+    public int size() {
+        return cards.size();
+    }
+}
