71bd9754c83bef54bea522c4942d9bba454da645
Iwona Bendig <iwona.bbbb@gmail.com>
2026-03-25 16:32:22 +0100

    fix error handling

--- backend/src/main/java/com/iwonabendig/blackjack/model/dto/ErrorResponseDTO.java
+++ backend/src/main/java/com/iwonabendig/blackjack/model/dto/ErrorResponseDTO.java
@@ -1,8 +1,10 @@
 package com.iwonabendig.blackjack.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class ErrorResponseDTO {
     private int status;
     private String message;
